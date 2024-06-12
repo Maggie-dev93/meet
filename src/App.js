4d39966af7b8ba666ae5b8a2b53cb79f79b21b80
Maggie-dev93 <maggieneubiglong@gmail.com>
2024-06-12 13:41:23 +0000

    4.4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';  // Import React
+import React, { useState, useEffect } from 'react';  // Import React, useState, and useEffect
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
+import { extractLocations, getEvents } from './api';
 
-const App = () => {
- return (
-   <div className="App">
-     <CitySearch />
-      <EventList />  
-  </div>
- );
+function App() {
+
+  const [events, setEvents] = useState([]);
+  const [noe, setNoe] = useState(32);
+  const [allLocations, setAllLocations] = useState([]);
+  const [currentCity, setCurrentCity] = useState('See all cities');
+
+  const fetchData = async() => {
+    const allEvents = await getEvents();
+    const filteredEvents = currentCity === 'See all cities' ? allEvents
+                                                            : allEvents.filter((event) => event.location === currentCity);
+    setEvents(filteredEvents.slice(0, noe));
+    setAllLocations(extractLocations(allEvents));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [currentCity]);
+
+  return (
+    <div className="App">
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity}/>
+      <NumberOfEvents />
+      <EventList events={events}/>
+    </div>
+  );
 }
 
 export default App;
+
--- src/components/Event.js
+++ src/components/Event.js
@@ -1,11 +1,25 @@
-import React from 'react';  // Import React
-// src/components/Event.js
+import { useState } from "react";
 
-const Event = () => {
+
+const  Event = ({event}) => {
+   const [showDetails, setShowDetails] = useState(false);
     return (
-      <li></li>
-    );
-  }
-  
-  export default Event;
+        <li className="event">
+            <h2>{event.summary}</h2>
+            <p>{event.created}</p>
+            <p>{event.location}</p>
+            <button className='showDetailsButton' 
+            onClick={() => setShowDetails(!showDetails)}>
+                {showDetails ? 'Hide Details' : 'Show Details'}
+            </button>
+            {showDetails ? (
+                        <div className="details">
+                        <h3>Event Details</h3>
+                        <p>{event.details}</p>
+                        </div>) : null}
+        </li>
+    )
+}
+
+export default Event;
   
