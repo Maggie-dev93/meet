6c29a191d843fbdcbe1472ab367849e3a63cba13
Maggie-dev93 <maggieneubiglong@gmail.com>
2024-06-12 13:56:53 +0000

    4.4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React, { useState, useEffect } from 'react';  // Import React, useState, and useEffect
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
-import { extractLocations, getEvents } from './api';
 
 function App() {
   return (
